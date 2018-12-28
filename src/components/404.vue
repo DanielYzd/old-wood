72a3d4279004b3cc8a793f9f8b6d7493a231251f
杨正东 <zhengdong.yang@geely.com>
2018-12-28 17:17:38 +0800

    add 404

--- src/components/404.vue
+++ src/components/404.vue
@@ -1,13 +1,45 @@
 <template>
-    <div>
-        404
+  <div>
+    <div class="container">
+      <div class="row pad-top text-center">
+        <div class="col-md-6 col-md-offset-3 text-center">
+          <h1>What have you done?</h1>
+          <h5>Now Go Back Using Below LInk</h5>
+          <span id="error-link">404?</span>
+          <h2>! ERROR DECETED !</h2>
+        </div>
+      </div>
+      <div class="row text-center">
+        <div class="col-md-8 col-md-offset-2">
+          <h3>
+            <i class="fa fa-lightbulb-o fa-5x"></i>
+          </h3>
+          <router-link :to="{ name:'Login'}">返回登录页</router-link>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
-<style lang="less">
-
+<style>
+/*=======================================
+    GENERAL STYLES
+==================================================*/
+body {
+  /* font-family: 'Nova Flat', cursive;
+    background-color: #B396FF;
+color: #fff; */
+}
+.pad-top {
+  padding-top: 60px;
+}
+.text-center {
+  text-align: center;
+}
+#error-link {
+  font-size: 150px;
+  padding: 10px;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -75,18 +75,23 @@ let routes = [{
             { path: '/Ue', component: Ue, name: 'Ue' },
         ]
     },
+    {
+        path: '*',
+        component: NotFound
+    }
 ]
 
 
 const router = new Router({
-    routes: routes
-})
-router.beforeEach((to, from, next) => {
-    if (to.matched.length === 0) { //如果未匹配到路由
-        next('/NotFound'); //如果上级也未匹配到路由则跳转登录页面，如果上级能匹配到则转上级路由
-    } else {
-        next(); //如果匹配到正确跳转
-    }
-});
+        routes: routes,
+        mode: 'history'
+    })
+    // router.beforeEach((to, from, next) => {
+    //     if (to.matched.length === 0) { //如果未匹配到路由
+    //         next('/NotFound'); //如果上级也未匹配到路由则跳转登录页面，如果上级能匹配到则转上级路由
+    //     } else {
+    //         next(); //如果匹配到正确跳转
+    //     }
+    // });
 
 export default router
